cae5fddf9937d7a7328536f978494bf5645022a8
Vatsal <vatsalpipalava@gmail.com>
2023-10-08 22:23:17 +0530

    Add Prac 4

---
+++ Prac4_Polyalphabetic.py
@@ -0,0 +1,35 @@
+# Function to generate the key in a cyclic manner
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+# Function to generate the ciphertext
+def cipherText(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) + ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+    return "".join(cipher_text)
+
+# Function to decrypt the ciphertext and get the original text
+def originalText(cipher_text, key):
+    orig_text = []
+    for i in range(len(cipher_text)):
+        x = (ord(cipher_text[i]) - ord(key[i]) + 26) % 26
+        x += ord('A')
+        orig_text.append(chr(x))
+    return "".join(orig_text)
+
+if __name__ == "__main__":
+    string = input("Enter the message: ").upper()  # Convert input to uppercase
+    keyword = input("Enter the key: ").upper()  # Convert input to uppercase
+    key = generateKey(string, keyword)
+    cipher_text = cipherText(string, key)
+    print("Ciphertext:", cipher_text)
+    print("Original/Decrypted Text:", originalText(cipher_text, key))
